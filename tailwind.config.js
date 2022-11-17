739cc237eaa0d4192d301b4a99ba568432f0ba04
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2022-11-18 04:41:48 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      maxWidth: {
+        '8xl': '1920px',
+      },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {},
+      textColor: {},
+      boxShadow: {},
+      lineHeight: {
+        'extra-loose': '2.2',
+      },
+      scale: {
+        120: '1.2',
       },
     },
   },
