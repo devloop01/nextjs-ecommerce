1e33016e1a5899009f12b6a2384c500b578dced1
Sikriti Dakua <destrodev123@gmail.com>
2022-11-24 02:23:52 +0530

    update: modify server routes

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,7 +1,7 @@
 import { inferAsyncReturnType } from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
 
-import { prisma } from '~/utils/prisma'
+import { prisma } from '~/server/db/client'
 
 export async function createContext(opts: trpcNext.CreateNextContextOptions) {
   return {
