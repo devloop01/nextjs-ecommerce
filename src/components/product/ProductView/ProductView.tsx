530ca15a6e7a3c7b2b2c6d94e1937c92768329fc
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2023-01-03 16:13:17 +0530

    added images

--- src/components/product/ProductView/ProductView.tsx
+++ src/components/product/ProductView/ProductView.tsx
@@ -1,6 +1,6 @@
 import type { Product } from '@prisma/client'
 
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import cn from 'clsx'
@@ -9,7 +9,7 @@ import s from './ProductView.module.css'
 
 import { QuantityButton, Container } from '~/components/ui'
 import { ChevronRightIcon } from '~/components/icons'
-import { ProductVarientCard } from '~/components/product'
+import { ProductVarientCardList } from '~/components/product'
 
 interface ProductViewProps {
   className?: string
@@ -25,8 +25,8 @@ const ProductView: React.FC<ProductViewProps> = ({ className, product }) => {
           <div>
             <div className="h-[600px] w-full bg-gray-300">
               <Image
-                src={product.defaultImage.url}
-                alt={product.defaultImage.alt ?? ''}
+                src={product.images[0].url}
+                alt={product.images[0].alt ?? ''}
                 width={512}
                 height={512}
                 className="h-full w-full object-contain"
@@ -48,15 +48,14 @@ const ProductView: React.FC<ProductViewProps> = ({ className, product }) => {
             </Link>
             <ChevronRightIcon className="w-4" />
             <span className="pointer-events-none text-gray-500">
-              {product.name}
+              {product.title}
             </span>
           </nav>
 
-          <span className="mt-4 text-4xl">{product.name}</span>
+          <span className="mt-4 text-4xl">{product.title}</span>
 
           <div className="mt-4 flex items-start gap-3">
-            <ProductVarientCard product={product} active />
-            <ProductVarientCard product={product} />
+            <ProductVarientCardList product={product} />
           </div>
 
           <div className="mt-4">
